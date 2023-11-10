70b74570ee5e4f13d67544c530d988c7998a59c3
morris88826 <morris88826@gmail.com>
2023-11-10 11:23:38 -0600

    solve the bug for add, and add delete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -57,9 +57,7 @@ class ItemsController < ApplicationController
   # DELETE /items/1 or /items/1.json
   def destroy
     @item.destroy
-
     respond_to do |format|
-      format.html { redirect_to items_url, notice: "Item was successfully destroyed." }
       format.json { head :no_content }
     end
   end
