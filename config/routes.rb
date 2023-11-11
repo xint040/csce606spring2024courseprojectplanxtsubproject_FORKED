ce94c946485d7d1c611d10b6545b04c331f070ac
morris88826 <morris88826@gmail.com>
2023-11-10 20:35:23 -0600

    test

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   # user sign up and login
   get 'sign_in', to: 'sessions#new'
   delete 'logout', to: 'sessions#destroy'
-  # if Rails.env.production?
-  get 'auth/events360/callback', to: 'sessions#create'
-  # end
+  if Rails.env.production?
+    get 'auth/events360/callback', to: 'sessions#create'
+  end
 end
