5b01b6aafecb964964fb7881b112180e606a12e5
morris88826 <morris88826@gmail.com>
2023-11-14 01:25:57 -0600

    update migrate

--- db/migrate/20231114062800_change_item_time_to_date_time_correct.rb
+++ db/migrate/20231114062800_change_item_time_to_date_time_correct.rb
@@ -2,11 +2,13 @@ class ChangeItemTimeToDateTimeCorrect < ActiveRecord::Migration[7.0]
   def change
     remove_column :items, :start_time
     remove_column :items, :end_time
+    remove_column :items, :breakdown_start_time
+    remove_column :items, :breakdown_end_time
 
     add_column :items, :setup_start_time, :datetime
     add_column :items, :setup_end_time, :datetime
 
-    change_column :items, :breakdown_start_time, :datetime
-    change_column :items, :breakdown_end_time, :datetime
+    add_column :items, :breakdown_start_time, :datetime
+    add_column :items, :breakdown_end_time, :datetime
   end
 end
