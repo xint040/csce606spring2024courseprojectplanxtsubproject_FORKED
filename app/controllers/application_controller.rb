2d066d862baee2624ec220b08eedc855db8c8bb7
lruff <louis.ruffino@tamu.edu>
2023-11-15 02:19:12 +0000

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :set_current_user
-  def set_current_user
-    # finds user with session data and stores it if present
-    Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
-  end
 end
