fe9016b933fe1d498be3b9188fc8584541000ab4
morris88826 <morris88826@gmail.com>
2023-11-24 10:26:41 -0800

    change migration

--- db/migrate/20231124161755_update_time.rb
+++ db/migrate/20231124161755_update_time.rb
@@ -1,10 +1,18 @@
 class UpdateTime < ActiveRecord::Migration[7.0]
   def change
-    change_column :steps, :start_time, :datetime
-    change_column :steps, :end_time, :datetime
-    change_column :steps, :break1_start_time, :datetime
-    change_column :steps, :break1_end_time, :datetime
-    change_column :steps, :break2_start_time, :datetime
-    change_column :steps, :break2_end_time, :datetime
+
+    remove_column :steps, :start_time
+    remove_column :steps, :end_time
+    remove_column :steps, :break1_start_time
+    remove_column :steps, :break1_end_time
+    remove_column :steps, :break2_start_time
+    remove_column :steps, :break2_end_time
+
+    add_column :steps, :start_time, :datetime
+    add_column :steps, :end_time, :datetime
+    add_column :steps, :break1_start_time, :datetime
+    add_column :steps, :break1_end_time, :datetime
+    add_column :steps, :break2_start_time, :datetime
+    add_column :steps, :break2_end_time, :datetime
   end
 end
