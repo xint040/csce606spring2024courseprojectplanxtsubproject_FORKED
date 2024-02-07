5015f1c855f036e2f9ce475ae1a7e436681f9392
Alex Wise <alexwise@Alexs-MacBook-Pro.local>
2024-02-06 21:04:03 -0600

    update files to work

--- db/schema.rb
+++ db/schema.rb
@@ -45,15 +45,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_29_104030) do
 
   create_table "steps", force: :cascade do |t|
     t.date "start_date"
+    t.integer "plan_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.datetime "start_time"
     t.datetime "end_time"
     t.datetime "break1_start_time"
     t.datetime "break1_end_time"
     t.datetime "break2_start_time"
     t.datetime "break2_end_time"
-    t.integer "plan_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
